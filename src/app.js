8a3369d5ea3829f52b29fc799bc4e30d7d108ca4
baekCode <sosaer88@gmail.com>
2020-08-11 19:10:25 +0900

    modify : 불필요한 코드 제거 및 라우터 적용

--- src/app.js
+++ src/app.js
@@ -3,18 +3,20 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import userRouter from './router';
-
+import routers from './routers';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 const app = express();
-
-const handleHome = (req, res) => res.send('Hi from Home!');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
-app.get('/', handleHome);
-app.use('/user', userRouter);
+
+app.use('/', globalRouter);
+app.use(routers.users, userRouter);
+app.use(routers.videos, videoRouter);
 
 export default app;
